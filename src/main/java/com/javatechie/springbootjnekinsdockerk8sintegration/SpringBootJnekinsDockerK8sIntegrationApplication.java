25f1df2a5e0aa105e919d5ff2ec4ab633c3ba186
thalarikalyan <thalarikalyaninfo@gmail.com>
2024-04-25 09:03:28 -0400

    first commit

---
+++ src/main/java/com/javatechie/springbootjnekinsdockerk8sintegration/SpringBootJnekinsDockerK8sIntegrationApplication.java
@@ -0,0 +1,13 @@
+package com.javatechie.springbootjnekinsdockerk8sintegration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootJnekinsDockerK8sIntegrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootJnekinsDockerK8sIntegrationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/javatechie/springbootjnekinsdockerk8sintegration/SpringBootJnekinsDockerK8sIntegrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javatechie.springbootjnekinsdockerk8sintegration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootJnekinsDockerK8sIntegrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
